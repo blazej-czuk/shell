b059a09e43c7c38d92d3bebe49f3d7f8027027cd
Blazej Czuk <blazej.czuk@palturai.com>
2024-08-19 10:13:26 +0200

    zsh plugin setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euEo pipefail
+
+# ZSH pplugin setup
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+grep -q "zsh-syntax-highlighting" ~/.zshrc || sed -i '/^plugins=/ s/)/ zsh-syntax-highlighting)/' ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+grep -q "zsh-autosuggestions" ~/.zshrc || sed -i '/^plugins=/ s/)/ zsh-autosuggestions)/' ~/.zshrc
+
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+grep -q "zsh-history-substring-search" ~/.zshrc || sed -i '/^plugins=/ s/)/ zsh-history-substring-search)/' ~/.zshrc
